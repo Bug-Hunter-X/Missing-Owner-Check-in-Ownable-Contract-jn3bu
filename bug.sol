e6585dd24d39c73eefafb7a8e6d1375079ddda9e
BugHunter-X <askzenai@gmail.com>
2024-12-17 20:46:04 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,7 @@
+function transferOwnership(newOwner) {
+  require(newOwner != address(0), "Ownable: new owner is the zero address");
+  emit OwnershipTransferred(_owner, newOwner);
+  _owner = newOwner;
+}
+
+// ... rest of the contract
